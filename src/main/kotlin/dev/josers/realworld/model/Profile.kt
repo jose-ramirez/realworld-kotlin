507bc4a5321b3247d514c039786fe111466fee05
Jose Ramirez <a30673083@gmail.com>
2020-08-15 17:11:57 -0300

    Refactoring profile class

--- src/main/kotlin/dev/josers/realworld/model/Profile.kt
+++ src/main/kotlin/dev/josers/realworld/model/Profile.kt
@@ -1,4 +1,7 @@
 package dev.josers.realworld.model
 
-data class Profile (var username: String, var bio: String?, var image: String?, var following: Boolean){
-}
+data class Profile (
+    var username: String,
+    var bio: String?,
+    var image: String?,
+    var following: Boolean)
